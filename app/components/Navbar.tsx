701c3e3b34739f798c01ce6fffde07b53d259c70
Om Kute <kuteom2006@gmail.com>
2025-02-16 20:19:15 +0530

    Responsive Sidebar Added

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,9 +5,10 @@ import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 
 import { UserButton } from "@clerk/nextjs";
-import { Menu, Sparkles } from "lucide-react";
+import { Sparkles } from "lucide-react";
 import { Poppins } from "next/font/google";
 import Link from "next/link";
+import { MobileSidebar } from "./MobileSidebar";
 
 const font = Poppins({
   weight: "600",
@@ -16,9 +17,9 @@ const font = Poppins({
 
 export function Navbar() {
   return (
-    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary">
+    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary h-16">
       <div className="flex items-center">
-        <Menu className="block md:hidden"></Menu>
+        <MobileSidebar/>
         <Link href="/">
           <h1
             className={cn(
